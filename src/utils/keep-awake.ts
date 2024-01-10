6ed70ed224a37fcb063ec8e9da835b634974b92a
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2024-01-10 17:29:26 +0000

    wip:

--- src/utils/keep-awake.ts
+++ src/utils/keep-awake.ts
@@ -2,18 +2,32 @@ import NoSleep from "nosleep.js";
 import { makeLogger } from "./log";
 
 let instance: NoSleep | null = null;
+let wakeLock: WakeLockSentinel | null = null;
 
 const logger = makeLogger("KeepAwake");
 
 export const KeepAwake = {
-  enable() {
+  async enable() {
     logger.log("enable");
-    instance = new NoSleep();
-    instance.enable();
+
+    if ("wakeLock" in navigator) {
+      navigator.wakeLock
+        .request("screen")
+        .then((_) => (wakeLock = _))
+        .catch(logger.error);
+    } else {
+      instance = new NoSleep();
+      instance.enable();
+    }
   },
 
   destroy() {
     logger.log("destroy");
+    if (wakeLock) {
+      wakeLock.release();
+      wakeLock = null;
+      return;
+    }
     if (!instance) return;
     instance.disable();
   },
